9f61ab9bd1a03655526be2da696dd93af5ead19b
Rami Chasygov <3c9wjqgw5@mozmail.com>
2023-09-15 16:16:54 +0300

    Export ToPublicKey trait

--- src/lib.rs
+++ src/lib.rs
@@ -5,6 +5,7 @@ mod datagram;
 mod tl_types;
 
 pub use client::Client;
+pub use crypto::ToPublicKey;
 
 pub type Result<T> = std::result::Result<T, Error>;
 pub type Error = Box<dyn std::error::Error>;
